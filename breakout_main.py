3654d19b8a385cc12d3fac905f00aaa7bedea039
Vinicius-Soares <vsdc.snf19@uea.edu.br>
2019-09-27 10:09:02 -0400

    linha superior

--- breakout_main.py
+++ breakout_main.py
@@ -26,17 +26,17 @@ ball.goto(0,-280)
 
 # Funcao para fazer os blocos (temporario)
 def blocks(col, xdir, ydir):
-    paddle = turtle.Turtle()
-    paddle.speed(0)
-    paddle.shape("square")
-    paddle.color(col)
-    paddle.shapesize(stretch_wid=0.5, stretch_len=2.5)
-    paddle.penup()
-    paddle.goto(xdir,ydir)
+    block = turtle.Turtle()
+    block.speed(0)
+    block.shape("square")
+    block.color(col)
+    block.shapesize(stretch_wid=0.5, stretch_len=2.5)
+    block.penup()
+    block.goto(xdir,ydir)
 
 # Blocos, matriz 8x10 (temporario) 
 xcor = -270
-ycor = 100
+ycor = 50
 colors = ["yellow","green","orange","red"]
 for i in range(8): # linhas
     x = colors[int(i/2)] # a cada duas linhas ele muda de cor
@@ -46,5 +46,13 @@ for i in range(8): # linhas
     ycor += 15
     xcor = -270
 
+# Linha superior
+line = turtle.Turtle()
+line.speed(0)
+line.shape("square")
+line.color("white")
+line.shapesize(stretch_wid=0.25, stretch_len=30)
+line.penup()
+line.goto(0,250)
 while True:
     screen.update()

8e97d53f7779d90294c7ab11a2e895ddb35e5893
HelderSchramm <hms.snf19@uea.edu.br>
2019-09-29 18:40:18 -0400

    raquete não entra mais na parede

--- breakout_main.py
+++ breakout_main.py
@@ -26,19 +26,19 @@ collide_walls
 
 def paddle_right():
     x = paddle.xcor()
-    if x < 300:
+    if x < 250:
         x += 20
     else:
-        x = 300
+        x = 250
     paddle.setx(x)
 
 
 def paddle_left():
     x = paddle.xcor()
-    if x > -300:
+    if x > -250:
         x -= 20
     else:
-        x = -300
+        x = -250
     paddle.setx(x)
 
 
